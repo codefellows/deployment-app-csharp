003c3b4f750eb84f3c048694b3efa36eac8909a4
Roger Huba <charm --wait>
2019-07-16 10:47:52 -0700

    update to deploy to aws

--- Program.cs
+++ Program.cs
@@ -20,5 +20,6 @@ namespace basic_web_app
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+                
     }
 }
